7948f55d346da04ee945e0d354e6d7b7fda1af43
tefuna <tefuna@gmail.com>
2018-05-11 11:42:40 +0900

    update.

--- src/main/java/com/tefuna/common/util/CodeUtil.java
+++ src/main/java/com/tefuna/common/util/CodeUtil.java
@@ -11,23 +11,23 @@ import com.tefuna.common.code.ICode;
  */
 public final class CodeUtil {
 
-	private CodeUtil() {
-		throw new UnsupportedOperationException();
-	}
+    private CodeUtil() {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 比較対象のcodeが比較元のコード定義との一致比較します。
-	 *
-	 * @param enumCode 比較元のコード定義enum
-	 * @param code 比較対象
-	 * @return 一致：true
-	 */
-	public static <E extends ICode> boolean isEquals(E enumCode, String code) {
-		if (enumCode == null) {
-			throw new NullPointerException("enumCode is null.");
-		}
+    /**
+     * 比較対象のcodeが比較元のコード定義との一致比較します。
+     *
+     * @param enumCode 比較元のコード定義enum
+     * @param code 比較対象
+     * @return 一致: true
+     */
+    public static <E extends ICode> boolean isEquals(E enumCode, String code) {
+        if (enumCode == null) {
+            throw new NullPointerException("enumCode is null.");
+        }
 
-		return Objects.equals(enumCode.getCode(), code);
-	}
+        return Objects.equals(enumCode.getCode(), code);
+    }
 
 }
